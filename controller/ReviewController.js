25daa2b972bbdb4632292c1c76c126e2388656e2
Kavishka <kavishkaimalsha@gmail.com>
2025-02-16 12:09:22 +0530

    Implement reviews delete part

--- controller/ReviewController.js
+++ controller/ReviewController.js
@@ -46,3 +46,59 @@ export const getReviews = (request, response) => {
         })
     }
 }
+
+export const deleteReview = (request, response) => {
+    const reviewId = request.params.reviewId
+    const email = request.params.email
+
+    if(request.user == null){
+        response.status(401).json({
+            message : "Unauthorize user please login again"
+        })
+        return
+    }
+
+    console.log(request.user.role);
+    
+
+    if(request.user.role == "admin"){
+        Review.deleteOne({
+            email : email,
+            _id : reviewId
+        }).then(() => {
+            response.json({
+                message : "Review deleted successfully"
+            })
+        }).catch((error) => {
+            response.json({
+                error : error
+            })
+        })
+    }
+
+    if(request.user.role == "customer"){
+        if(request.user.email != email){
+            response.status(401).json({
+                message : "You can't perform this action"
+            })
+            return
+        }
+
+        Review.deleteOne({
+            _id : reviewId,
+            email : email
+        }).then(() => {
+            response.json({
+                message : "Review deleted successfully"
+            })
+        }).catch((error) => {
+            response.json({
+                error : error
+            })
+        })
+    }
+    
+}
+
+// "_id": "67ab06753e2b81eb510261a0",
+//         "email": "exampleuser@gmail.com",
--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,10 +1,10 @@
 import express from "express"
-import { addReview, getReviews } from "../controller/ReviewController.js"
+import { addReview, deleteReview, getReviews } from "../controller/ReviewController.js"
 
 const reviewRoutes = express.Router()
 
 reviewRoutes.post('/', addReview)
 reviewRoutes.get('/', getReviews)
-//userRoutes.delete('/:reviewId/:email')
+reviewRoutes.delete('/:reviewId/:email', deleteReview)
 
 export default reviewRoutes
